21dff4311fae8690c13fe3729427b98d9ba00d67
diegogranados-10 <diegogranados143@gmail.com>
2025-11-05 13:15:40 -0400

    Add Main method to ComplexStack for testing expression evaluation and output results

--- week02/teach/ComplexStack.cs
+++ week02/teach/ComplexStack.cs
@@ -1,19 +1,42 @@
 ﻿public static class ComplexStack {
-    public static bool DoSomethingComplicated(string line) {
+    
+    public static void Main()
+    {
+        var lines = new string[]
+        {
+            "(a == 3 or (b == 5 and c == 6))",
+            "(students]i].Grade > 80 and students[i].Grade < 90)",
+            "(robot[id + 1].Execute(.Pass() || (!robot[id * (2 + i)].Alive && stormy) || (robot[id - 1].Alive && lavaFlowing))",
+
+        };
+
+        foreach (var line in lines)
+        {
+            var result = DoSomethingComplicated(line);
+            Console.WriteLine($"{line} -> {result}");
+        }
+    }
+    public static bool DoSomethingComplicated(string line)
+    {
         var stack = new Stack<char>();
-        foreach (var item in line) {
-            if (item is '(' or '[' or '{') {
+        foreach (var item in line)
+        {
+            if (item is '(' or '[' or '{')
+            {
                 stack.Push(item);
             }
-            else if (item is ')') {
+            else if (item is ')')
+            {
                 if (stack.Count == 0 || stack.Pop() != '(')
                     return false;
             }
-            else if (item is ']') {
+            else if (item is ']')
+            {
                 if (stack.Count == 0 || stack.Pop() != '[')
                     return false;
             }
-            else if (item is '}') {
+            else if (item is '}')
+            {
                 if (stack.Count == 0 || stack.Pop() != '{')
                     return false;
             }
